f285676335908cf2aa78d02657a7cad20cce5ebf
BlockChainDev <kevtheappdev@gmail.com>
2019-05-07 12:04:52 +0100

    [key]
    - Add false R value to test

--- key/key_test.go
+++ key/key_test.go
@@ -19,12 +19,17 @@ func TestDidReceive(t *testing.T) {
 	R.ScalarMultBase(&r)
 
 	pubKey0 := k.PublicKey().StealthAddress(r, 0)
-	pubKey1 := k.PublicKey().StealthAddress(r, 2)
+	pubKey1 := k.PublicKey().StealthAddress(r, 1)
 
 	privKey0, ok := k.DidReceiveTx(R, *pubKey0, 0)
 	assert.True(t, ok)
-	privKey1, ok := k.DidReceiveTx(R, *pubKey1, 2)
+	privKey1, ok := k.DidReceiveTx(R, *pubKey1, 1)
 	assert.True(t, ok)
+
+	var randPoint ristretto.Point
+	randPoint.Rand()
+	_, ok = k.DidReceiveTx(randPoint, *pubKey1, 1)
+	assert.False(t, ok)
 
 	var expectedPubKey0 ristretto.Point
 	expectedPubKey0.ScalarMultBase(privKey0)
