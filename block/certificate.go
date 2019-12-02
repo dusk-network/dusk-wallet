f1b54760c7e638871cb100bd8a66e43570c3a6fc
Jules de Smit <julesdesmit@gmail.com>
2019-12-02 14:03:49 +0100

    add missing fields to the block certificate struct

--- block/certificate.go
+++ block/certificate.go
@@ -6,18 +6,22 @@ import (
 
 // Certificate defines a block certificate made as a result from the consensus.
 type Certificate struct {
+	Round             uint64
+	Step              uint8 // Step the agreement terminated at (1 byte)
+	BlockHash         []byte
 	StepOneBatchedSig []byte // Batched BLS signature of the block reduction phase (33 bytes)
 	StepTwoBatchedSig []byte
-	Step              uint8  // Step the agreement terminated at (1 byte)
 	StepOneCommittee  uint64 // Binary representation of the committee members who voted in favor of this block (8 bytes)
 	StepTwoCommittee  uint64
 }
 
 func EmptyCertificate() *Certificate {
 	return &Certificate{
+		Round:             0,
+		Step:              0,
+		BlockHash:         make([]byte, 32),
 		StepOneBatchedSig: make([]byte, 33),
 		StepTwoBatchedSig: make([]byte, 33),
-		Step:              0,
 		StepOneCommittee:  0,
 		StepTwoCommittee:  0,
 	}
@@ -29,15 +33,23 @@ func (c *Certificate) Equals(other *Certificate) bool {
 		return false
 	}
 
+	if c.Round != other.Round {
+		return false
+	}
+
+	if c.Step != other.Step {
+		return false
+	}
+
+	if !bytes.Equal(c.BlockHash, other.BlockHash) {
+		return false
+	}
+
 	if !bytes.Equal(c.StepOneBatchedSig, other.StepOneBatchedSig) {
 		return false
 	}
 
 	if !bytes.Equal(c.StepTwoBatchedSig, other.StepTwoBatchedSig) {
-		return false
-	}
-
-	if c.Step != other.Step {
 		return false
 	}
 
