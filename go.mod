7de0a370c2552af735929ed319167fad02bf90ec
Jules de Smit <julesdesmit@gmail.com>
2019-12-18 12:57:03 +0100

    add test ensuring data integrity of tx records when encoded and decoded

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dusk-network/dusk-zkproof v0.0.0-20190727103229-8b0c008561ee
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/onsi/ginkgo v1.8.0 // indirect
 	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/stretchr/testify v1.4.0
@@ -14,6 +15,7 @@ require (
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
 	golang.org/x/sys v0.0.0-20190924135425-2f72d4f06240 // indirect
 	golang.org/x/text v0.3.2 // indirect
+	gotest.tools v2.2.0+incompatible
 )
 
 go 1.13
---
+++ txrecords/txrecords_test.go
@@ -0,0 +1,40 @@
+package txrecords_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/dusk-network/dusk-wallet/transactions"
+	"github.com/dusk-network/dusk-wallet/txrecords"
+	"gotest.tools/assert"
+)
+
+// Ensure integrity of data between encoding and decoding
+func TestEncodeDecodeTxRecord(t *testing.T) {
+	r := &txrecords.TxRecord{
+		Direction:    txrecords.In,
+		Timestamp:    time.Now().Unix(),
+		TxType:       transactions.BidType,
+		Amount:       7172727182793,
+		UnlockHeight: 300000,
+		Recipient:    "pippo",
+	}
+
+	buf := new(bytes.Buffer)
+	if err := txrecords.Encode(buf, r); err != nil {
+		t.Fatal(err)
+	}
+
+	decoded := &txrecords.TxRecord{}
+	if err := txrecords.Decode(buf, decoded); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, r.Direction, decoded.Direction)
+	assert.Equal(t, r.Timestamp, decoded.Timestamp)
+	assert.Equal(t, r.TxType, decoded.TxType)
+	assert.Equal(t, r.Amount, decoded.Amount)
+	assert.Equal(t, r.UnlockHeight, decoded.UnlockHeight)
+	assert.Equal(t, r.Recipient, decoded.Recipient)
+}
